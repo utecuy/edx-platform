f88260b3c81ce1f897329475a34c5f36ffe9cb8a
melvinsoft <melvinsoft@gmail.com>
2015-11-04 16:42:17 -0300

    change comment

--- lms/djangoapps/student_account/views.py
+++ lms/djangoapps/student_account/views.py
@@ -362,7 +362,7 @@ def account_settings_context(request):
         'duplicate_provider': None,
         'fields': {
             'country': {
-                'options': [['UY', 'Uruguay'],], # para obtener todos list(countries)
+                'options': [['UY', 'Uruguay'],], # for all options list(countries)
             }, 'gender': {
                 'options': [(choice[0], _(choice[1])) for choice in UserProfile.GENDER_CHOICES],  # pylint: disable=translation-of-non-string
             }, 'language': {
